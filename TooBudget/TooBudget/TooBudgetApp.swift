ae7271259750a013b65321dc6d92869cc7b5d720
Engineer Jorge Paiz <jorgepaiz007@gmail.com>
2024-02-02 16:51:01 -0600

    Base Application with basic Splash screen

---
+++ TooBudget/TooBudget/TooBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooBudgetApp.swift
+//  TooBudget
+//
+//  Created by Jorge Paiz on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TooBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
